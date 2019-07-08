73967dff439c94638cf47b9dfbbdadd408349859
Jacob Weightman <jacob.weightman@smartthings.com>
2019-07-08 15:33:17 -0500

    Bump to 17.0 with xcode 10.2.1

--- SourceryUtils/Sources/Version.swift
+++ SourceryUtils/Sources/Version.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public struct Version {
     public let value: String
-    public static let current = Version(value: inUnitTests ? "Major.Minor.Patch" : "0.16.1")
+    public static let current = Version(value: inUnitTests ? "Major.Minor.Patch" : "0.17.0")
 }
 
 public var inUnitTests = NSClassFromString("XCTest") != nil
